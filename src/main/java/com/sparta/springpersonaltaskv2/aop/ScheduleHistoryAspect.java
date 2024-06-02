a33bf50951424870c684e900d8d45641cf40bb48
ydh5017 <ydh5017@naver.com>
2024-06-02 16:56:47 +0900

    AOP 수정

--- src/main/java/com/sparta/springpersonaltaskv2/aop/ScheduleHistoryAspect.java
+++ src/main/java/com/sparta/springpersonaltaskv2/aop/ScheduleHistoryAspect.java
@@ -8,12 +8,13 @@ import com.sparta.springpersonaltaskv2.enums.AspectType;
 import com.sparta.springpersonaltaskv2.repository.ScheduleHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
+/**
+ * 일정 관련 로깅 AOP 구성
+ */
 @Aspect
 @Component
 @RequiredArgsConstructor
@@ -22,18 +23,57 @@ public class ScheduleHistoryAspect {
 
     private final ScheduleHistoryRepository scheduleHistoryRepository;
 
+    /**
+     * Before : 일정 등록 시도 전 로깅 AOP
+     * <p>
+     *     메서드가 실행되기 전에 Advice를 실행한다.
+     * </p>
+     * @param requestDto 일정 정보
+     * @param user 회원 정보
+     */
     @Before(value = "execution(* com.sparta.springpersonaltaskv2.service.ScheduleService.createSchedule(..)) && args(requestDto, user)", argNames = "requestDto,user")
     public void beforeAddSchedule(ScheduleRequestDto requestDto, User user) {
         log.info("[일정 등록 시도] 작성자 : {}, 제목 : {}", user.getUsername(), requestDto.getContent());
     }
 
-    @AfterReturning(value = "execution(* com.sparta.springpersonaltaskv2.service.ScheduleService.createSchedule(..))", returning = "result")
-    public void afterAddScheduleSuccess(ScheduleResponseDto result) {
-        ScheduleHistory scheduleHistory = new ScheduleHistory(result, AspectType.AFTER_RETURNING);
-        scheduleHistoryRepository.save(scheduleHistory);
-        log.info("[일정 등록 성공] 작성자 : {}, 일정 ID : {}", result.getWriter(), result.getId());
+    /**
+     * After : 일정 등록 메서드 실행 후 로깅 AOP
+     * <p>
+     *     메서드가 실행되고 반환되기 전에 Advice를 실행한다.
+     * </p>
+     * @param joinPoint Aspect가 적용되는 시점
+     * @param user 회원 정보
+     * @param requestDto 일정 정보
+     */
+    @After(value = "execution(* com.sparta.springpersonaltaskv2.service.ScheduleService.createSchedule(..)) && args(requestDto, user)", argNames = "joinPoint,user,requestDto")
+    public void afterAddSchedule(JoinPoint joinPoint, User user, ScheduleRequestDto requestDto) {
+        log.info("[일정 등록 후] 경로 {}, 작성자 : {}, 제목 : {}",
+                joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName(),
+                requestDto.getTitle(),
+                user.getUsername());
     }
 
+    /**
+     * AfterReturning : 일정 등록 성공 로깅 AOP
+     * <p>
+     *      메서드가 정상적으로 실행되고 반환된 후에 Advice를 실행한다.
+     * </p>
+     * @param responseDto 일정 정보
+     */
+    @AfterReturning(value = "execution(* com.sparta.springpersonaltaskv2.service.ScheduleService.createSchedule(..))", returning = "responseDto")
+    public void afterAddScheduleSuccess(ScheduleResponseDto responseDto) {
+        ScheduleHistory scheduleHistory = new ScheduleHistory(responseDto, AspectType.AFTER_RETURNING);
+        scheduleHistoryRepository.save(scheduleHistory);
+        log.info("[일정 등록 성공] 작성자 : {}, 일정 ID : {}", responseDto.getWriter(), responseDto.getId());
+    }
+
+    /**
+     * AfterThrowing : 일정 등록 실패 로깅 AOP
+     * <p>
+     *     메서드에서 예외 발생 시 Advice를 실행한다.
+     * </p>
+     * @param e 예외
+     */
     @AfterThrowing(value = "execution(* com.sparta.springpersonaltaskv2.service.ScheduleService.createSchedule(..))", throwing = "e")
     public void afterAddScheduleFailure(Throwable e) {
         log.error("[일정 등록 실패] message : {}", e.getMessage());
